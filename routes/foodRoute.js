e8046f6710ab75a590caea0a909f317c37334132
BinhKT <120490978+ktbinh2206@users.noreply.github.com>
2024-10-01 02:37:29 +0700

    oke ne

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const { addFood, listFood, removeFood } = require('../controllers/foodController.js');
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const foodRouter = express.Router();
 
-//Image Storage Engine (Saving Image to uploads folder & rename it)
+// Thư mục lưu trữ tệp tạm thời
+const uploadDir = '/tmp/uploads';
 
+// Kiểm tra và tạo thư mục nếu nó không tồn tại
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Image Storage Engine (Lưu trữ hình ảnh vào thư mục tạm thời và đổi tên)
 const storage = multer.diskStorage({
-    destination: 'uploads',
+    destination: uploadDir,  // Thay đổi đường dẫn đến thư mục tạm
     filename: (req, file, cb) => {
-        return cb(null,`${Date.now()}${file.originalname}`);
+        return cb(null, `${Date.now()}-${file.originalname}`);
     }
-})
+});
 
-const upload = multer({ storage: storage})
+const upload = multer({ storage: storage });
 
-foodRouter.get("/list",listFood);
-foodRouter.post("/add",upload.single('image'),addFood);
-foodRouter.post("/remove",removeFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/add", upload.single('image'), addFood);
+foodRouter.post("/remove", removeFood);
 
 module.exports = foodRouter;
